5972b3b7c0287f331e62c0cbaf7d522ff450f41e
hvrs <hvrsttk@gmail.com>
2022-03-25 11:02:15 +0700

    Сделали четвёртый коммент

--- app/src/main/java/com/example/projectgitgladunov/MainActivity.java
+++ app/src/main/java/com/example/projectgitgladunov/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //сделали первый коммент
+        //Сделали четвёртый коммент
     }
 }
